0f6920d5a307aca7c73b1ab06759716ea42e7428
haiderali9823-xSomaK <haiderali9823@outlook.com>
2022-09-27 19:25:22 +0100

    adding routes

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -17,14 +17,28 @@ res.status(400).json(err)
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try{
+    const categoryData = await Category.findOne({
+      include: [Product]
+    })
+    res.status(200).json(categoryData)
+  }
+  catch(err){
+res.status(400).json(err)
+  }
 });
 
-router.post('/', (req, res) => {
-  // create a new category
-});
+router.post('/', async (req, res) => {
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (error) {
+    res.status(400).json(error)
+  }
+ });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
