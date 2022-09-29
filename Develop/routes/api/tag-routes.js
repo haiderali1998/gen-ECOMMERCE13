ed4da0dbcc42e296ca9a375ea91c5bf9e0676c91
haiderali9823-xSomaK <haiderali9823@outlook.com>
2022-09-29 13:16:05 +0100

    id get

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -25,6 +25,9 @@ router.get('/:id', async (req, res) => {
   // be sure to include its associated Product data
   try {
     const tagData = await Tag.findOne({
+      where: {
+        id: req.params.id,
+      },
       include: {
         model: Product,
         through: ProductTag
@@ -37,8 +40,14 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/',async (req, res) => {
   // create a new tag
+  try {
+    const tagData = await Tag.create(req.body);
+    res.status(200).json(tagData);
+  } catch (error) {
+    res.status(400).json(error)
+  }
 });
 
 router.put('/:id', (req, res) => {
